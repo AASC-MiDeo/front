7aea64de46c72741148fb144e7ceca01036c0781
Hyrge <ekek7478@gmail.com>
2024-06-02 06:07:23 +0900

    firebase 연동

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
